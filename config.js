58d89f05ed379a1df9dc7e49760d24ccc6c12e7a
elRazer <31215264+elRazer@users.noreply.github.com>
2024-03-17 20:57:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  token: 'MTIxNDY3MTAxNTA0OTY5OTQxOA.GWJ4IV.YfqYBT1Ji1D0B_ABVl2TXgoh7TT754K1d63wH0',
-  clientId: '1214671015049699418',
-  clientSecret: 'dhNzyXFq-we3AzUMm26ousHK6Q6fW1KU',
-  redirectUri: 'https://restoretest.ernstvoll.de/verify',
+  token: 'xxxx',
+  clientId: 'xxxx',
+  clientSecret: 'xxxx',
+  redirectUri: 'https://xxxx/verify',
   mongoURI: 'mongodb://localhost:27017/discordbotdb',
 };
  
